16a06c0d4b4518658e26039703544f0f7ba45375
The Linux Choice <34893261+thelinuxchoice@users.noreply.github.com>
2018-09-27 22:55:52 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,26 @@
+# Install dependencies for GetCookies
+# Author: https://github.com/thelinuxchoice/getcookies
+
+printf "\e[1;77m[\e[0m\e[1;92m+\e[0m\e[1;77m] Updating and downloading Mingw-w64\n"
+apt-get update;
+apt-get install -y mingw-w64;
+
+wget https://curl.haxx.se/download/curl-7.61.1.tar.gz
+
+if [[ -e curl-7.61.1.tar.gz ]]; then
+
+tar vzxf curl-7.61.1.tar.gz -C /opt/
+else
+
+printf "e[1;93m[!] Download Error!\e[0m\n"
+exit 1
+fi
+
+if [[ -d /opt/curl-7.61.1 ]]; then
+cd /opt/curl-7.61.1;
+./configure --host=i686-w64-mingw32 --build=i686-pc-linux-gnu --prefix=/usr/i686-w64-mingw32/ --enable-static --disable-shared;
+make && make install;
+else
+printf "e[1;93m[!] Error! Check \"tar\" command.\e[0m\n"
+exit 1
+fi
